4d53235113eed043ac9bc4e92772cd61bab430cf
Scott Miller <39809971+millers123456789@users.noreply.github.com>
2018-11-28 18:19:45 -0600

    Update app.js

--- java_script_hw/app.js
+++ java_script_hw/app.js
@@ -5,34 +5,50 @@
 
 var ufo = data;
 var submit = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+
+var populate = (dataInput) => {
+
+  dataInput.forEach(ufo_sightings => {
+      var row = tbody.append("tr");
+      columns.forEach(column => row.append("td").text(ufo_sightings[column])
+      )
+  });
+}
+populate(ufo);
 
 submit.on("click", function() {
 
+  
  
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputValue = inputElement.property("value").trim();
  // Prevent the page from refreshing
  d3.event.preventDefault();
 
- var filteredData = ufo.filter(sighting => sighting.datetime === inputValue);
+ var filteredData = ufo.filter(ufo => ufo.datetime === inputValue);
   
+ tbody.html("");
 
-
-var tbody = d3.select("tbody");
+//var tbody = d3.select("tbody");
 console.log(ufo);
 
 
 
 // BONUS: Refactor to use Arrow Functions!
-filteredData.forEach((sighting) => {
+filteredData.forEach((ufo) => {
     var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
+    Object.entries(ufo).forEach(([key, value]) => {
       var cell = tbody.append("td");
       cell.text(value);
     });
   });
+  
 });
+
+
